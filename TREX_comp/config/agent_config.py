986a6414939fc3ba06afc964a7a65eb12d862f10
pmt <s243292@dtu.dk>
2026-06-10 18:26:25 +0200

    fix reward

--- TREX_comp/config/agent_config.py
+++ TREX_comp/config/agent_config.py
@@ -161,7 +161,7 @@ agent_configs = {
     "IDQN_MM2": {
         'agent': IDQN,
         'state': states.drq_multimodal_norm,
-        'reward': rewards.wait_mm2_norm,
+        'reward': rewards.wait_multimodal_delta_sclip,
         'max_distance': 200,
         'BATCH_SIZE': 32,
         'GAMMA': 0.99,
