557f144f5886dc0bf811b75edb9dba888f4c0183
pmt <s243292@dtu.dk>
2026-06-10 18:55:50 +0200

    fix

--- TREX_comp/config/map_config.py
+++ TREX_comp/config/map_config.py
@@ -255,6 +255,17 @@ map_configs = {
         'start_time': 0,
         'end_time': 3600,
         'warmup': 0
+    },
+    'kbh_j1_442m': {
+        'lights': [],
+        'net': 'environments/kbh_j1_442m/kbh_j1_4.4.2.sumocfg',
+        'route': None,
+        'step_length': 10,
+        'yellow_length': 3,
+        'step_ratio': 1,
+        'start_time': 0,
+        'end_time': 3600,
+        'warmup': 0
     }
 
 }
